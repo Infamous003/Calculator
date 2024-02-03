3f8a2927eaa73de281f29749916b9bb52309ad8e
Infamous003 <arfathsyed0312@gmail.com>
2024-02-03 22:49:05 +0530

    Rewrote the entire script.js, changed my approach. The code is more cleaner and readable. Now the user can continue to perform operations with the previous result, there is a limit to the number of digits on the screen and only two operands can be operated at a time.

--- script.js
+++ script.js
@@ -2,21 +2,72 @@ const date = new Date();
 let footer = document.querySelector('footer');
 footer.innerHTML = `Syed Mehdi ${date.getFullYear()}`;
 footer.style.cssText = 'font-family:Arial;';
+/*---------------------------------------------------*/ 
 
-let num1 = 0,
-    operator, 
-    num2 = 0,
-    result = 0;
-let expression = '';
 let userInput = document.querySelector('.user-input');
-let numButtons = document.querySelectorAll('.num-btn');
-let optrButtons = document.querySelectorAll('.operator-btn');
-let buttons = document.querySelectorAll('button');
-let equals = document.querySelector('.equal');
-let clear = document.querySelector('.clear');
-let deletebtn = document.querySelector('.delete');
-let isOperatorPresent = false;
+let allButtons = document.querySelectorAll('button');
+let clearButton = document.querySelector('.clear');
+let equalsButton = document.querySelector('.equal');
+let deleteButton = document.querySelector('.delete');
+let operands = {
+  num1: '',
+  num2: '',
+  operator: null
+};
 
+allButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if(button.classList.contains('num-btn')){
+      userInput.innerText += button.innerText;
+      if(!operands.operator){
+        operands.num1 += button.innerText;
+      }else{
+        operands.num2 += button.innerText;
+      }
+    }
+
+    if(button.classList.contains('operator-btn')){
+      if(!operands.operator){
+        operands.operator = button.innerText;
+        limitDigit();
+        userInput.innerText += operands.operator;
+      }else{
+        calculate();
+      }
+    }
+    limitDigit(userInput.innerText);
+  })
+});
+
+//----------CLEAR BUTTON----------//
+clearButton.addEventListener('click', () => {
+  userInput.innerText = '';
+  operands.num1 = operands.num2 = '';
+  operands.operator = null;
+});
+
+//----------DELETE BUTTON---------//
+deleteButton.addEventListener('click', () => {
+  let enteredString = userInput.innerText;
+
+  console.log(enteredString.charAt(enteredString.length-1));
+  if(enteredString.charAt(enteredString.length-1) == operands.operator){
+    operands.operator = null;
+    userInput.innerText = enteredString.slice(0, enteredString.length-1);
+  }
+  else if(enteredString.charAt(enteredString.length-1) == (operands.num2).charAt(operands.num2.length-1)){
+    let newNum2 = operands.num2.slice(0, operands.num2.length-1);
+    operands.num2 = newNum2;
+    userInput.innerText = enteredString.slice(0, enteredString.length-1);
+  }
+  else if(enteredString.charAt(enteredString.length-1) == (operands.num1).charAt(operands.num1.length-1)){
+    let newNum1 = operands.num1.slice(0, operands.num1.length-1);
+    operands.num1 = newNum1;
+    userInput.innerText = enteredString.slice(0, enteredString.length-1);
+  }
+});
+
+//--------------------FUNCTIONS-----------------------//
 function add(a, b) { return a + b }
 function sub(a, b) { return a - b }
 function mul(a, b) { return a * b }
@@ -33,92 +84,35 @@ function operate(num1, operator, num2) {
     case "/":
       return div(num1, num2);
     default: 
-      return ;
+      return 0;
   }
-  
 }
-
-clear.addEventListener('click', () => {
-  userInput.innerText = '';
-  num1 = num2 = 0;
-})
-
-equals.addEventListener('click', () => {
-  result = operate(+num1, operator, +num2);
-  console.log("Result: " + result);
-  userInput.innerText = Number.isInteger(result) ? result : result.toFixed(3);
-  num1 = result;
-  result = 0;
-  num2 = 0;
-  operator = null;
-});
-
-deletebtn.addEventListener('click' , () => {
-  let enteredString = userInput.innerText;
-  let len = enteredString.length;
-
-  if(enteredString.charAt(len-1) === operator){    
-    userInput.innerText = enteredString.slice(0, len-1);
-    operator = null;
-    for(let i = 0; i < userInput.innerText.length; i++){
-      if(Number(userInput.innerText.charAt(i))){
-        continue;
-      }
-      operator = userInput.innerText.charAt(i);
-      return;
+//----------CALCULATE-------------//
+function calculate() {
+  let result = operate(+operands.num1, operands.operator, +operands.num2);
+  if(result == "Infinity"){
+    userInput.innerText = "BRUH";
+    operands.num1 = operands.num2 = '';
+    operands.operator = null;
+    return;
+  }
+  if(Number(result)){
+    if(Number.isInteger(result)){
+      userInput.innerText = result;
+    }else if(!Number.isInteger(result)){
+      userInput.innerText = (+Math.fround(result)).toFixed(1);
     }
-    return ;
+    operands.num1 = `${result}`;
+    operands.num2 = '';
+    operands.operator = null;
+    return;
   }
-  let characterAt = enteredString.charAt(len-1);
-  
-  let newString = enteredString.slice(0, len-1);
-
-  if(characterAt == num2.toString().charAt(num2.length-1)){
-    let newNum2 = num2;
-    num2 = newNum2.slice(0, num2.length-1);
-    userInput.innerText = enteredString.slice(0, len-1);
-    return ;
-  }
-  if(characterAt == num1.toString().charAt(num1.length-1)){
-    let newNum1 = num1;
-    num1 = newNum1.slice(0, num1.length-1);
-    userInput.innerText = enteredString.slice(0, len-1);
-    return ;
-  }
-});
-
-showText();
-
-
-
-function showText() {
-  let span = document.createElement('span');
-  numButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      if(operator) {
-        userInput.innerText += btn.innerText;
-        num2 += btn.innerText;
-        return ;
-      }else{
-        userInput.innerText += btn.innerText;
-        num1 += btn.innerText;
-        return ;
-      }
-    })
-  });
-  optrButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      operator = btn.innerText;
-      span.innerText = btn.innerText;
-      userInput.appendChild(span);
-    })
-  })
-
 }
 
-function isInputLarge(){
-  if(userInput.innerText.length > 5){
-    return true;
+function limitDigit(str) {
+  if(userInput.innerText.length > 10){
+    alert("Stop!");
+    userInput.innerText = str.slice(0, str.length-1);
+    return ;
   }
-  return false;
 }
